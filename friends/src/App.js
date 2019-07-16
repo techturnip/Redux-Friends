3739474222108e133ff30f25b0fb42e6e7352a08
Tyler Turnipseed <tyler@techturnip.us>
2019-07-16 12:08:41 -0400

    Implemented Materialize CSS in App component

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
+// Materialize CSS
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css'
+// Styles
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Friends app</h1>
-      </header>
-    </div>
-  )
+class App extends Component {
+  componentDidMount() {
+    // Auto initialize Materialize CSS
+    M.AutoInit()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Friends app</h1>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App
